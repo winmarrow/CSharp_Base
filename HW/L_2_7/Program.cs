6c740187222174fa8798d1971225e97f6515f6ac
winmarrow <winmarrow@gmail.com>
2018-06-22 23:08:52 +0300

    Small fixes

---
+++ HW/L_2_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CH = SharedLib.ConsoleHelpers.ConsoleHelper;
+
+namespace L_2_7
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CH.SetConsoleColor();
+            CH.SetConsoleOutputEncoding();
+
+
+            Console.ReadKey();
+        }
+    }
+}
